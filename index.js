e46a6622223d0bc2050900c5a3f19d665d7f4dbe
MiguelGlzAliat <144168149+MiguelGlzAliat@users.noreply.github.com>
2023-09-15 13:06:14 -0600

    quice

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -11,10 +12,38 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/productos', (req, res) => {
+  const { page } = req.query;
+  const limit = page || 100;
+  const productos = [];
+  for (let index = 0; index < limit; index++) {
+    productos.push({
+      nombre: faker.commerce.productName(),
+      precio: parseInt(faker.commerce.price(), 10),
+      imagen: faker.image.imageUrl()
+    });
+  }
+  res.json(productos);
+})
+
+app.get('/productos/:id', (req, res) => {
+  const { id } = req.params;
+  res.json([id, {
+    nombre: "producto #2",
+    precio: 300
+  }]);
+})
+
+app.get('/categorias/:id_cat/productos/:id_prod', (req, res) => {
+  const { id_cat, id_prod } = req.params;
   res.json({
-    nombre: "Producto #1",
-    precio: 10000
-  });
+    id_cat,
+    id_prod
+  })
+})
+
+app.get('/usuarios', (req, res) => {
+  const { limit, offset } = req.query;
+  (limit && offset)?res.json({limit, offset}):res.send("Ay caramba, no hay nada");
 })
 
 app.listen(port, () => {
